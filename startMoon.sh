e78fcdccfcdee5b8e3be18fe818242eb8ff6a84f
Alwin Grossbach <alwin.grossbach@outlook.de>
2021-03-28 11:20:26 +0200

    add crontabscript
    for checking every 5 minutes if the service is running

--- startMoon.sh
+++ startMoon.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-pkill -f MoonBot.py
+# Name of program in ps-list
+NAME="MoonBot*"
 
-yes "" | nohup python3 ./MoonBot.py &amp;
+# check if running
+if ( ps -a | grep "$NAME" )
+then
+echo "$NAME is running..."
+else
+echo "$NAME NOT running! Restarting..."
+python3 ./MoonBot.py
+fi
 
 exit 0
